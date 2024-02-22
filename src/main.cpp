3c860790964fdf5cd186a218e9d9ff84ccec3ebc
Noah <noahsun0418@gmail.com>
2024-02-22 15:08:57 -0500

    🔧 Changes for new bot - Updated ports to new robot - Added defines to be able to change drive controls between different drivers quickly - Added defines to be able to change drive code and auton for skills

--- src/main.cpp
+++ src/main.cpp
@@ -11,70 +11,66 @@
 
 using namespace lib16868C;
 
+#define SKILLS
+#define ANSON
+// #define WINSTON
+
 void initialize() {
 	pros::lcd::initialize();
 
-	odometry.init();
+	// odometry.init();
+	inertial.reset(true);
+	chassis.coast();
 }
 
-void disabled() {}
+void disabled() {
+	#ifdef SKILLS
+	horiHang.retract();
+	#endif
+}
 
 void competition_initialize() {}
 
 void autonomous() {
 	int st = pros::millis();
 
+	#ifndef SKILLS
 	// nearAWPBar();
-	farAWP();
+	// farAWP();
 	// farAWPBar();
 	// nearRush();
-	// skillsStart();
+	// nearDisrupt();
+	#endif
 	// skills();
-	// skills2();
+	#ifdef SKILLS
+	skills2();
+	#endif
 
-	std::cout << "Auton took " << pros::millis() - st << " ms" << std::endl;
+	// std::cout << "Auton took " << pros::millis() - st << " ms" << std::endl;
 }
 
 void opcontrol() {
-	int kickerSpd = 100;
-	bool displayKicker = false;
-	pros::Task displayController([&] {
-		uint st = pros::millis();
-		master.clear();
-		while (true) {
-			std::stringstream l0; l0 << std::fixed << std::setprecision(1) << "t: " << (pros::millis() - st) / 1000.0 << "  B: " << pros::battery::get_capacity();
-			master.setText(0, 0, l0.str());
-			pros::delay(50);
-				
-			if (!displayKicker) {
-				master.setText(1, 0, "L: " + std::to_string((int) leftDrive.getTemperature()) + "  R: " + std::to_string((int) rightDrive.getTemperature()));
-				pros::delay(50);
-				master.setText(2, 0, "I: " + std::to_string((int) intake.getTemperature()) + "  K: " + std::to_string((int) kicker.getTemperature()));
-				pros::delay(50);
-			} else {
-				master.setText(2, 0, "v: " + std::to_string((int) kickerSpd) + "  T: " + std::to_string((int) kicker.getTemperature()) + "  ");
-				pros::delay(50);
-			}
-		}
-	});
-
-	// skillsStart();
+	#ifdef SKILLS
+	skillsStart();
+	#endif
 
 	okapi::ControllerButton intakeTgl(okapi::ControllerDigital::R1);
-	okapi::ControllerButton outtakeTgl(okapi::ControllerDigital::R2);
+	okapi::ControllerButton outtakeTgl(okapi::ControllerDigital::L1);
 	okapi::ControllerButton kickerTgl(okapi::ControllerDigital::left);
-	okapi::ControllerButton leftWingTgl(okapi::ControllerDigital::A);
-	okapi::ControllerButton rightWingTgl(okapi::ControllerDigital::Y);
-	okapi::ControllerButton frontWingsTgl(okapi::ControllerDigital::L1);
-	okapi::ControllerButton vertWingsTgl(okapi::ControllerDigital::L2);
+	okapi::ControllerButton leftWingTgl(okapi::ControllerDigital::L2);
+	okapi::ControllerButton rightWingTgl(okapi::ControllerDigital::R2);
+	okapi::ControllerButton frontWingsTgl(okapi::ControllerDigital::A);
+	okapi::ControllerButton vertWingsTgl(okapi::ControllerDigital::Y);
 	okapi::ControllerButton horiHangTgl(okapi::ControllerDigital::X);
 
-	okapi::ControllerButton increaseKickerSpd(okapi::ControllerDigital::up);
-	okapi::ControllerButton decreaseKickerSpd(okapi::ControllerDigital::down);
-	okapi::ControllerButton switchDisplay(okapi::ControllerDigital::right);
-
 	int intakeDir = 0;
+	int kickerSpd = 110;
+	#ifndef SKILLS
 	bool matchloading = false;
+	#endif
+	#ifdef SKILLS
+	bool matchloading = true;
+	#endif
 	while (true) {
 		// double forward = master.getAnalog(okapi::ControllerAnalog::leftY);
 		// double turn = master.getAnalog(okapi::ControllerAnalog::rightX);
@@ -83,23 +79,34 @@ void opcontrol() {
 		double right = master.getAnalog(okapi::ControllerAnalog::rightY);
 		chassis.driveTank(left, right);
 
+		#ifdef WINSTON
+		if (intakeTgl.isPressed()) intakeDir = 1;
+		else if (outtakeTgl.isPressed()) intakeDir = -1;
+		else intakeDir = 0;
+		#endif
+		#ifdef ANSON
 		if (intakeTgl.changedToPressed()) intakeDir = intakeDir == 1 ? 0 : 1;
-		if (outtakeTgl.changedToPressed()) intakeDir = intakeDir == -1 ? 0 : -1;
+		else if (outtakeTgl.changedToPressed()) intakeDir = intakeDir == -1 ? 0 : -1;
 		intake.moveVoltage(intakeDir * 12000);
+		#endif
 
 		if (kickerTgl.changedToPressed()) matchloading = !matchloading;
 		kicker.move(matchloading * kickerSpd);
-		if (increaseKickerSpd.changedToPressed()) if (kickerSpd + 10 <= 200) kickerSpd += 10;
-		if (decreaseKickerSpd.changedToPressed()) if (kickerSpd - 10 >= 0) kickerSpd -= 10;
-
-		if (switchDisplay.changedToPressed()) { master.clearLine(1); master.clearLine(2); displayKicker = !displayKicker; }
-
+		
 		if (leftWingTgl.changedToPressed()) leftWing.toggle();
 		if (rightWingTgl.changedToPressed()) rightWing.toggle();
 		if (frontWingsTgl.changedToPressed()) { leftWing.toggle(); rightWing.toggle(); }
 		if (vertWingsTgl.changedToPressed()) vertWings.toggle();
 
+		#ifndef SKILLS
 		if (horiHangTgl.changedToPressed()) horiHang.toggle();
+		#endif
+		#ifdef SKILLS
+		if (horiHangTgl.changedToPressed()) {
+			matchloading = false;
+			intakeDir = -1;
+		}
+		#endif
 
 		pros::delay(50);
 	}
