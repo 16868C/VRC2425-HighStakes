d79a073a44ad6200b2ffbb270fa8c812bb9c30e8
Noah <noahsun0418@gmail.com>
2024-06-25 02:36:32 -0400

    Logger class
    - Created the logger class; still requires to be tested but compiles
    - Uses mutex
    - Flushes after every data entry
    - Still have not implemented logger into other classes
    - Still requires to be documented

--- include/16868C/util/logger.hpp
+++ include/16868C/util/logger.hpp
@@ -1,5 +1,9 @@
 #pragma once
+#include <fstream>
+#include <initializer_list>
 #include <string>
+#include <vector>
+#include "pros/rtos.hpp"
 
 #define DEBUG
 #define ERROR
@@ -23,4 +27,36 @@ template<typename ... Args> void printError(const char* fmt, Args ... args) {
 	fprintf(stderr, fmt, Argument(args) ...);
 	#endif
 }
+
+class Logger {
+	public:
+		Logger(const std::string file, std::initializer_list<std::string> cols, uint writeInterval = 100);
+		~Logger();
+
+		void setData(std::string col, std::string value);
+		std::string getData(std::string col);
+		std::vector<std::pair<std::string, std::string>>* getAllData();
+
+		std::string getFileName();
+		std::ofstream& getFileOutput();
+
+		void setWriteInterval(uint writeInterval);
+		uint getWriteInterval();
+
+	private:
+		std::string fileName;
+		std::ofstream fileOut;
+
+		std::vector<std::pair<std::string, std::string>> data;
+
+		uint writeInterval { 100 };
+
+		pros::Mutex dataMutex;
+
+		pros::Task writeTask = NULL;
+		static void writeThread(void* params);
+
+		static std::vector<std::pair<std::string, std::string>>::iterator find(std::vector<std::pair<std::string, std::string>> v, std::string key);
+		static bool contains(std::vector<std::pair<std::string, std::string>> v, std::string key);
+};
 } // namespace lib16868C
---
+++ src/16868C/util/logger.cpp
@@ -0,0 +1,116 @@
+#include "logger.hpp"
+#include <algorithm>
+
+using namespace lib16868C;
+
+Logger::Logger(const std::string file, std::initializer_list<std::string> cols, uint writeInterval) {
+	fileName = file;
+
+	fileOut = std::ofstream(file);
+	fileOut << "time,";
+	for (std::string c : cols) {
+		// Headers
+		fileOut << c << ",";
+
+		// Adding the columns to the map
+		if (!contains(data, c)) {
+			data.emplace_back(c, "");
+		}
+	}
+	fileOut.flush();
+
+	this->writeInterval = writeInterval;
+
+	writeTask = pros::Task(writeThread, this, TASK_PRIORITY_MIN, TASK_STACK_DEPTH_DEFAULT, ("Logger::" + fileName).c_str());
+}
+Logger::~Logger() {
+	fileOut.flush();
+	fileOut.close();
+
+	writeTask.remove();
+	writeTask = NULL;
+}
+
+void Logger::setData(std::string col, std::string value) {
+	if (!dataMutex.take(100)) {
+		printError("[Logger::%s] Mutex timeout - unable to update data", fileName);
+		return;
+	}
+	if (!contains(data, col)) {
+		printError("[Logger::%s] Column %s not found - was not initialized in constructor", fileName, col);
+		return;
+	}
+
+	find(data, col)->second = value;
+	
+	dataMutex.give();
+}
+std::string Logger::getData(std::string col) {
+	if (!dataMutex.take(100)) {
+		printError("[Logger::%s] Mutex timeout - unable to update data", fileName);
+		return NULL;
+	}
+	if (!contains(data, col)) {
+		printError("[Logger::%s] Column %s could not be found, was not initialized in constructor", fileName, col);
+		return NULL;
+	}
+
+	std::string ret = find(data, col)->second;
+	dataMutex.give();
+	return ret;
+}
+std::vector<std::pair<std::string, std::string>>* Logger::getAllData() {
+	if (!dataMutex.take(100)) {
+		printError("[Logger::%s] Mutex timeout - unable to update data", fileName);
+		return nullptr;
+	}
+
+	std::vector<std::pair<std::string, std::string>>* ret = &data;
+	dataMutex.give();
+	return ret;
+}
+
+std::string Logger::getFileName() {
+	return fileName;
+}
+std::ofstream& Logger::getFileOutput() {
+	return fileOut;
+}
+
+void Logger::setWriteInterval(uint writeInterval) {
+	this->writeInterval = writeInterval;
+}
+uint Logger::getWriteInterval() {
+	return writeInterval;
+}
+
+void Logger::writeThread(void* params) {
+	Logger* logger = (Logger*) params;
+
+	uint32_t time = pros::millis();
+	while (true) {
+		if (!logger->dataMutex.take(100)) {
+			printError("[Logger::%s] Mutex timeout - unable to update data", logger->fileName);
+			return;
+		}
+
+		// Output data to file
+		logger->fileOut << pros::millis() - time << ",";
+		for (auto it = logger->data.begin(); it != logger->data.end(); it++) {
+			logger->fileOut << it->second << ",";
+		}
+		logger->fileOut.flush();
+
+		logger->dataMutex.give();
+
+		pros::Task::delay_until(&time, logger->writeInterval);
+	}
+}
+
+std::vector<std::pair<std::string, std::string>>::iterator find(std::vector<std::pair<std::string, std::string>> v, std::string key) {
+	return std::find_if(v.begin(), v.end(),
+					[&key](const std::pair<std::string, std::string>& e) { return e.first == key; });
+}
+bool contains(const std::vector<std::pair<std::string, std::string>> v, std::string key) {
+	return find(v, key) != v.end();
+}
